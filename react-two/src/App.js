be3048aafaa509837a7114e55e8e9d9b16e81ea1
Leontine Claassen <leon.claassen00@gmail.com>
2021-10-03 22:41:44 +0200

    fontSizeButton and imageSlider

---
+++ react-two/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Slider from './Slider'
+import SliderData from "./SliderData";
+
+export default function App() {
+  
+
+  const [fontSize, setFontSize] = useState(16);
+
+  return (
+    <>
+      <div className='container'>
+      <div className="exampleParagraph">
+        <div className='fontButtons'>
+        <button className='fontButton' onClick={() => setFontSize(fontSize + 2)}>bigger +</button>
+        <button className='fontButton' onClick={() => setFontSize(fontSize - 2)}>smaller -</button>
+        </div>
+        <p style={{
+          fontSize: `${fontSize}px`
+        }}>
+          Cupcake ipsum dolor sit amet ice cream. I love candy biscuit sweet
+          roll biscuit. Jelly I love liquorice chupa chups bonbon. Tootsie roll
+          jelly-o sesame snaps cake dragée liquorice muffin toffee pastry.
+          Cupcake cheesecake biscuit I love danish dragée donut muffin apple
+          pie. Donut cake I love apple pie cookie apple pie powder. Pie danish
+          dragée sesame snaps candy canes.
+        </p>
+      </div>
+      
+        <Slider slides={SliderData}/>
+        </div>
+    </>
+  );
+}
---
+++ react-two/src/SliderData.js
@@ -0,0 +1,25 @@
+const SliderData = [
+    {
+        image:'https://cdn.pixabay.com/photo/2017/08/06/06/58/food-2589686__340.jpg',
+        alt: 'sweets'
+       
+    },
+    {
+        image:'https://images.unsplash.com/photo-1570476922354-81227cdbb76c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c3dlZXRzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        alt: 'sweets'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1516747773440-e114ee0d3c07?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGNhbmRpZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        alt: 'sweets'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1541717872011-9d16b87a5551?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE4fHxjb29raWVzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        alt: 'sweets'
+    },
+    {
+        image:'https://images.unsplash.com/photo-1516919549054-e08258825f80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fHN3ZWV0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        alt: 'sweets'
+    }
+]
+
+export default SliderData
