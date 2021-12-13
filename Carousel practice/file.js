e55b527d7d1ffa2eb85cd0d796052ef8b3dcb988
LeontineC <79845918+LeontineC@users.noreply.github.com>
2021-12-13 15:41:32 +0100

    responsiveness

--- Carousel practice/file.js
+++ Carousel practice/file.js
@@ -13,7 +13,7 @@ const slideWidth = slides[1].getBoundingClientRect().width;
 console.log(slideWidth);
 
 
-//setting slides in row//
+//setting slides in row and //
 slides.forEach((slide, index) => {
       slide.style.left = slideWidth * index + "px";
 });
@@ -30,8 +30,9 @@ moveSlides = (carousel, currentSlide, targetSlide) => {
         console.log(targetSlide)
 };
 
-switchIndicators = () => {
-
+switchIndicators = (currentButton, targetButton) => {
+    currentButton.classList.remove('active-btn');
+    targetButton.classList.add('active-btn');
 };
 
 removeAddArrows = (prevArrow, nextArrow, slides, targetIndex) => {
@@ -56,8 +57,12 @@ prevArrow.addEventListener('click', (e) => {
     console.log(prevIndex);
     console.log(currIndex);
 
+    const currentButton = indicators.querySelector('.active-btn');
+    const prevButton = currentButton.previousElementSibling;
+
     moveSlides(carousel, currentSlide, previousSlide);
     removeAddArrows(prevArrow, nextArrow, slides, prevIndex);
+    switchIndicators(currentButton, prevButton);
 });
 
 nextArrow.addEventListener('click', (e) => {
@@ -69,10 +74,28 @@ nextArrow.addEventListener('click', (e) => {
     console.log(nextIndex);
     console.log(currIndex);
 
+    const currentButton = indicators.querySelector('.active-btn');
+    const nextButton = currentButton.nextElementSibling;
+
     moveSlides(carousel, currentSlide, nextSlide);
     removeAddArrows(prevArrow, nextArrow, slides, nextIndex);
+    switchIndicators(currentButton, nextButton);
 });
 
 indicators.addEventListener('click', (e) => {
+    const targetButton = e.target.closest('button');       //eventListener on entire div / e.target is a thing in js
+    if (!targetButton) {                                     //event  click now on buttons inside div or any other element indicated in brackets
+      return;         //prevents event triggering on any click besides buttons in div
+    }
 
+    const currentSlide = carousel.querySelector('.active-item');
+    const currentButton = indicators.querySelector('.active-btn');
+
+    const btnIndex = buttons.findIndex(btn => btn === targetButton);   // finds index of button clicked on
+    const targetSlide = slides[btnIndex];
+    console.log(targetSlide);
+
+    switchIndicators(currentButton, targetButton);
+    moveSlides(carousel, currentSlide, targetSlide);
+    removeAddArrows(prevArrow, nextArrow, slides, btnIndex);
 });
