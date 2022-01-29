fa9886b9b173675d858d73bcda86b48c4472ee7e
LeontineC <79845918+LeontineC@users.noreply.github.com>
2022-01-29 17:14:48 +0100

    backToTop function

---
+++ BackToTop ProgressBar/file.js
@@ -0,0 +1,34 @@
+const sectionOne = document.querySelector('.sectionOne');
+const sectionArrow = document.querySelector('.sectionArrow');
+const arrow = document.querySelector('.arrow');
+
+//hide and reveal backToTop section
+const navOptions = {
+ root: null,
+ threshold: 1.0,
+ rootMargin: '400px'
+};
+
+const sectionObserver = new IntersectionObserver(function(entries, sectionObserver) {
+    entries.forEach((entry) => {
+          if(entry.isIntersecting) {
+            sectionArrow.classList.add('hidden');
+          } else {
+              sectionArrow.classList.remove('hidden');
+          }
+        
+    })
+
+}, navOptions);
+
+sectionObserver.observe(sectionOne);
+
+// function to go back to top of page
+
+backToTop = () => {
+    document.body.scrollIntoView({
+        behavior: 'smooth',
+    });
+}
+
+arrow.addEventListener('click', backToTop);
