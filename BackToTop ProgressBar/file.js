a281d272582866bc6f06f2e7fe1b3caee0447d1d
LeontineC <79845918+LeontineC@users.noreply.github.com>
2022-01-29 17:23:23 +0100

    back to top function

--- BackToTop ProgressBar/file.js
+++ BackToTop ProgressBar/file.js
@@ -1,34 +1,36 @@
-const sectionOne = document.querySelector('.sectionOne');
-const sectionArrow = document.querySelector('.sectionArrow');
-const arrow = document.querySelector('.arrow');
+const sectionOne = document.querySelector(".sectionOne");
+const sectionArrow = document.querySelector(".sectionArrow");
+const arrow = document.querySelector(".arrow");
 
 //hide and reveal backToTop section
 const navOptions = {
- root: null,
- threshold: 1.0,
- rootMargin: '400px'
+  root: null,
+  threshold: 1.0,
+  rootMargin: "400px",
 };
 
-const sectionObserver = new IntersectionObserver(function(entries, sectionObserver) {
-    entries.forEach((entry) => {
-          if(entry.isIntersecting) {
-            sectionArrow.classList.add('hidden');
-          } else {
-              sectionArrow.classList.remove('hidden');
-          }
-        
-    })
-
-}, navOptions);
+const sectionObserver = new IntersectionObserver(function (
+  entries,
+  sectionObserver
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      sectionArrow.classList.add("hidden");
+    } else {
+      sectionArrow.classList.remove("hidden");
+    }
+  });
+},
+navOptions);
 
 sectionObserver.observe(sectionOne);
 
 // function to go back to top of page
 
 backToTop = () => {
-    document.body.scrollIntoView({
-        behavior: 'smooth',
-    });
-}
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
 
-arrow.addEventListener('click', backToTop);
+arrow.addEventListener("click", backToTop);
