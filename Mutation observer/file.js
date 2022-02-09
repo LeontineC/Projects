b6b687f177246a1f83f70e7457f2c0719d3f273d
LeontineC <79845918+LeontineC@users.noreply.github.com>
2022-02-09 17:40:36 +0100

    rewritten

--- Mutation observer/file.js
+++ Mutation observer/file.js
@@ -1,21 +1,33 @@
-const first = document.querySelector('.first');
-const second = document.querySelector('.second');
-const parent = document.querySelector('.wrapper');
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+const parent = document.querySelector(".wrapper");
 
 
 setInterval(() => {
-    first.innerHTML = Math.floor(Math.random()*100) + 1;
+  first.textContent = Math.floor(Math.random() * 100) + 1;
 }, 1000);
 
-const mutationObserver = new MutationObserver(entries => {
+/*const mutationObserver = new MutationObserver(entries => {
     console.log(entries);
     if(MutationRecord) {
         second.innerHTML = first.innerHTML;
     } else {
         return;
     }
-});
-   
+}); */
+
+changeSibling = () => {
+  if (MutationRecord) {
+    second.textContent = first.textContent;
+  } else {
+    return;
+  }
+};
+
+const mutationObserver = new MutationObserver(changeSibling);
+
+mutationObserver.observe(parent.children[0], { childList: true });
 
 
-mutationObserver.observe(parent.children[0], { childList: true})
+
+
