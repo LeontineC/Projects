d49484e0670660d2f07ed87986f429c8393b8b44
LeontineC <79845918+LeontineC@users.noreply.github.com>
2022-02-13 18:39:25 +0100

    gsap nightsky first

---
+++ GSAP nightsky/file.js
@@ -0,0 +1,25 @@
+gsap.to(".dot", {
+  duration: 3,
+  ease: "bounce",
+  opacity: 0.2,
+  stagger: { each: 1, repeat: -1 },
+});
+
+gsap.to(".star", {
+  duration: 4,
+  ease: "slow",
+  opacity: 0.2,
+  stagger: { each: 1, repeat: -1 },
+});
+
+gsap.to(".fallingstar", {
+  duration: 2,
+  ease: "sine",
+  y: 800,
+  x: -800,
+  stagger: {
+    each: 1,
+    repeat: -1,
+    repeatDelay: 10,
+  },
+});
